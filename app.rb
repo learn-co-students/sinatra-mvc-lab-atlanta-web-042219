baa05badc73188c26c06f3150da2ada50fd36c4b
Han Xu <hanxu27@gmail.com>
2019-05-13 20:23:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        pig = PigLatinizer.new()
+        @stuff = pig.piglatinize(params[:user_phrase])
+        erb :output
+    end
 end
