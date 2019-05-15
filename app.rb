0e46f362a82f0091944655982918e38c15998af7
seerosecode <keshalakemusic@gmail.com>
2019-05-15 15:24:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    text = PigLatinizer.new
+    @new_text = text.piglatinize(params[:user_phrase])
+    erb :piglatinized
+  end
+
+
 end
